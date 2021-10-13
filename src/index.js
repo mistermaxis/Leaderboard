c863b7e26a87c32559dbbf48e169bfdaa6ec1689
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2021-10-13 20:36:18 -0300

    Change alignment of submit button.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import App from './app.js';
 import './style.css';
 
-//const scoreApp = new App();
+const scoreApp = new App();
 
-//scoreApp.start();
-
-fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
-    body: JSON.stringify({
-        name: "Game",
-    }),
-    method: "POST",
-    headers: {
-        "content-type": "application/json",
-    },
-})
-.then(response => response.json())
-.then(response => console.log(response));
+scoreApp.start();
