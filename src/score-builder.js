0d9d8e815c910e0605725191a3b6edf3907f2106
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2021-10-14 14:09:03 -0300

    Fix linter errors.

--- src/score-builder.js
+++ src/score-builder.js
@@ -1,34 +1,33 @@
 class ScoreBuilder {
-    #item;
+  #item;
 
-    #name;
+  #name;
 
-    #score;
+  #score;
 
-    constructor() {
-      this.#item = null;
-      this.#name = null;
-      this.#score = null;
-    }
+  constructor() {
+    this.#item = null;
+    this.#name = null;
+    this.#score = null;
+  }
 
-    build(list, scores) {
-      scores.forEach((scoreItem, index) => {
-        setTimeout(() => {
-          this.#item = document.createElement('li');
-          this.#item.classList.add('score-item');
+  build(list, scores) {
+    scores.forEach((scoreItem, index) => {
+      setTimeout(() => {
+        this.#item = document.createElement('li');
+        this.#item.classList.add('score-item');
+        this.#name = document.createElement('span');
+        this.#score = document.createElement('span');
 
-          this.#name = document.createElement('span');
-          this.#score = document.createElement('span');
-  
-          this.#name.innerText = `${scoreItem.user}: `;
-          this.#score.innerText = scoreItem.score;
-  
-          this.#item.appendChild(this.#name);
-          this.#item.appendChild(this.#score);
-          list.appendChild(this.#item);
-        }, 250 * index);
-      });
-    }
+        this.#name.innerText = `${scoreItem.user}: `;
+        this.#score.innerText = scoreItem.score;
+
+        this.#item.appendChild(this.#name);
+        this.#item.appendChild(this.#score);
+        list.appendChild(this.#item);
+      }, 250 * index);
+    });
+  }
 }
 
 export default ScoreBuilder;
