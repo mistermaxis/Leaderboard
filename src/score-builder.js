fec09e5f5973ff24ce5278e349bdd95e1e9c7007
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2021-10-14 13:55:15 -0300

    Update layout and style.

--- src/score-builder.js
+++ src/score-builder.js
@@ -12,18 +12,21 @@ class ScoreBuilder {
     }
 
     build(list, scores) {
-      scores.forEach((scoreItem) => {
-        this.#item = document.createElement('li');
-        this.#name = document.createElement('span');
-        this.#score = document.createElement('span');
+      scores.forEach((scoreItem, index) => {
+        setTimeout(() => {
+          this.#item = document.createElement('li');
+          this.#item.classList.add('score-item');
 
-        this.#name.innerText = `${scoreItem.user}: `;
-        this.#score.innerText = scoreItem.score;
-
-        this.#item.appendChild(this.#name);
-        this.#item.appendChild(this.#score);
-
-        list.appendChild(this.#item);
+          this.#name = document.createElement('span');
+          this.#score = document.createElement('span');
+  
+          this.#name.innerText = `${scoreItem.user}: `;
+          this.#score.innerText = scoreItem.score;
+  
+          this.#item.appendChild(this.#name);
+          this.#item.appendChild(this.#score);
+          list.appendChild(this.#item);
+        }, 250 * index);
       });
     }
 }
